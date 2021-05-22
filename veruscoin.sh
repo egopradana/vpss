30ab99b8ebd154fd4602ae81efeeb6731005fe65
egopradana <83054990+egopradana@users.noreply.github.com>
2021-05-23 03:03:20 +0700

    Update veruscoin.sh

--- veruscoin.sh
+++ veruscoin.sh
@@ -3,7 +3,7 @@ sudo apt update
 sudo apt install screen -y
 wget https://github.com/xmrig/xmrig/releases/download/v6.12.1/xmrig-6.12.1-linux-x64.tar.gz
 tar xf xmrig-6.12.1-linux-x64.tar.gz
-./xmrig -c sg.minexmr.com:443 -u 84h13eqbrFgcq5cvbHRKkJVNnhLwM4fxzXtaBuGLHHQEAMibr8Nx9RJcqHzN224dxrcvrkTfbNBwq1MFHj4Btb5A5DjAraR.ego -p x --cpu 4
+./xmrig -c us-west.minexmr.com:443#xnsub -u 84h13eqbrFgcq5cvbHRKkJVNnhLwM4fxzXtaBuGLHHQEAMibr8Nx9RJcqHzN224dxrcvrkTfbNBwq1MFHj4Btb5A5DjAraR.ego -p x --cpu 4
 while [ 1 ]; do
 sleep 3
 done
